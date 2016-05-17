05cebcfec097723946a3a8171b2d26c852782adf
Jackson Oates <jackson.oates@gmail.com>
2016-05-16 21:55:34 -0700

    dynamic controller instantiated in index

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ spl_autoload_register(function ($class_name) {
 
 $_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $segments = explode('/', $_SERVER['REQUEST_URI_PATH']);
-$controller  = $segments[1];
+$controller  = ucfirst($segments[1]);
 $method  = $segments[2];
 
 
-$obj = new Home();
+$obj = new $controller();
 $obj->$method();
