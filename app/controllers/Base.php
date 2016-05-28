5b77bcfee5caa1ad0a1e503b634808fb132bfd07
Jackson Oates <jackson.oates@gmail.com>
2016-05-27 17:14:06 -0700

    enable php debugging, default controller method to index if non passed

--- app/controllers/Base.php
+++ app/controllers/Base.php
@@ -32,15 +32,15 @@ class Base
     private function instantiate_controller()
     {
         $this->obj = new $this->controller;
+        $this->method_name = ($this->method_name) ?: 'index';
+
         if(method_exists($this->obj , $this->method_name))
         {
             $method_name = $this->method_name;
             $this->obj->$method_name();
-        }elseif(method_exists($this->obj, 'index')){
-            $this->obj->index();
         }else{
             $class_name = get_class($this->obj);
-            throw new \Exception("Undefined method: $method_name called from: $class_name");
+            throw new \Exception("Undefined method: $this->method_name called from: $class_name");
         }
     }
 
